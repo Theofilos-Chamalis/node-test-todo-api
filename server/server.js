d5fd99ca2f0719f4ea0a1b3b7632612b519f2e0c
Theofilos-Chamalis <theofxam@gmail.com>
2017-11-19 17:26:19 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
-//Import express and body-parser
+//Import express, body-parser and the ObjectID from native mongodb driver so we can use the isValid method of it
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID} = require('mongodb');
 
 //Import mongoose which is connected to mongo and our models
-var {
-    mongoose
-} = require('./db/mongoose.js');
-var {
-    Todo
-} = require('./models/todo');
-var {
-    User
-} = require('./models/user');
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// Create an express instance and set a port variable
+//Create an express instance and set a port variable
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -29,16 +24,41 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
-        res.status(400).send(e);
+        res.status(400).send(e.message);
     });
 });
 
-//Setup a route for get requests at /todos endpoint
+//Setup a route for get requests of all todos at /todos endpoint
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        //Send this as a property inside a JSON for more tweakability in the future
+        res.send({
+            todos
+        });
     }, (e) => {
-        res.status(400).send(e);
+        //We could not send the error for security reasons
+        res.status(400).send(e.message);
+    });
+});
+
+//Setup a route for get requests of a single todo at /todos/id endpoint
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        //Using return to prevent further function execution
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo === null) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch((er) => {
+        res.status(400).send(er.message);
     });
 });
 
