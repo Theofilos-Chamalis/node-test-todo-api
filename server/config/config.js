237091c2d0d970e85ae429c3e7fd8f7e26795f2e
Theofilos-Chamalis <theofxam@gmail.com>
2017-12-02 15:28:57 +0200

    Minor change to the config file

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+//For development and testing get the configuration 
+//(port and mongoDB URI) from a JSON file
+var env = process.env.NODE_ENV || "development";
+
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
